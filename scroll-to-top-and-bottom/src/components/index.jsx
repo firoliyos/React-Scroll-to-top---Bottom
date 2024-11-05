8f7bf792822f4fb67d89ef15ab329a84c36dd5a3
firoliyos <firatesfaye44@gmail.com>
2024-11-05 16:48:24 +0300

    done

--- scroll-to-top-and-bottom/src/components/index.jsx
+++ scroll-to-top-and-bottom/src/components/index.jsx
@@ -1,11 +1,23 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import useFetch from "./use-fetch";
 
 export default function ScrollToTopAndBottom() {
   const { data, error, pending } = useFetch(
-    "https://dummyjson.com/products",
+    "https://dummyjson.com/products?limit=200",
     {}
   );
+
+  const bottomRef = useRef(null)
+  function handleScrollToBottom() {
+    bottomRef.current.scrollIntoView({ behavior: 'smooth' })
+  }
+  function handleScrollToTop() {
+    window.scrollTo({
+        top: 0, 
+        left : 0, 
+        behavior: 'smooth'
+    })
+  }
 
   if (pending) {
     return <h1>Loading...</h1>;
@@ -16,20 +28,21 @@ export default function ScrollToTopAndBottom() {
   }
 
   return (
-    <div>
-      <h1>SCROLL TO TO AND BOTTOM APP</h1>
+    <div style={{padding: '10px', margin: "20px"}}>
+      <h1>SCROLL TO TOP AND BOTTOM APP</h1>
       <h3>This is the Top section</h3>
-      <button>Scroll to Bottom</button>
+      <button onClick={handleScrollToBottom} style={{padding: '10px', margin: "20px", background:'black', color: 'yellow'}}>Scroll to Bottom</button>
       <ul style={{listStyle: 'none'}}>
        {
         data && data.products && data.products.length ? 
         data.products.map((item) => 
-            <li>{item.title}</li>
+            <li style={{padding: '10px', background: 'aliceblue', color: 'green' }}>{item.title}</li>
         )
         : null
        }
       </ul>
-      <button>Scroll to Top</button>
+      <button onClick={handleScrollToTop} style={{padding: '10px', margin: "20px", background:'black', color: 'yellow'}}>Scroll to Top</button>
+      <div ref={bottomRef}></div>
       <h3>This is Bottom section!</h3>
     </div>
   );
